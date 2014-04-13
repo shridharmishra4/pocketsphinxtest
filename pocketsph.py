7232113e0694eb38d5ffa9c5a72573a6af494cc1
shridharmishra4 <shridharmishra4@gmail.com>
2014-04-13 13:11:34 +0530

    added live speech recognition capability

--- pocketsph.py
+++ pocketsph.py
@@ -24,20 +24,75 @@
 
 import sys
 from pocketsphinx import Decoder
+import pyaudio
+import wave
+import os
+
+def record():
+	
+
+	chunk = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 1
+	RATE = 44100
+	RECORD_SECONDS = 5
+	WAVE_OUTPUT_FILENAME = "livewav.wav"
+	
+	p = pyaudio.PyAudio()
+	
+	stream = p.open(format = FORMAT,
+	                channels = CHANNELS,
+	                rate = RATE,
+	                input = True,
+	                frames_per_buffer = chunk)
+	
+	print "* recording"
+	os.system("aplay -D hw:1,0 beep_hi.wav")
+	
+	all = []
+	for i in range(0, RATE / chunk * RECORD_SECONDS):
+	    data = stream.read(chunk)
+	    all.append(data)
+	os.system("aplay -D hw:1,0 beep_lo.wav")
+	
+	print "* done recording"
+	
+	stream.close()
+	p.terminate()
+	
+	# write data to WAVE file
+	data = ''.join(all)
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(data)
+	wf.close()
+	
+
  
 if __name__ == "__main__":
  
    hmdir = "/usr/share/pocketsphinx/model/hmm/en_US/hub4wsj_sc_8k"
    lmdir = "/usr/share/pocketsphinx/model/lm/en_US/hub4.5000.DMP"
    dictd = "/usr/share/pocketsphinx/model/lm/en_US/cmu07a.dic"
-   wavfile = "/home/shridhar/web/Java Tutorial for Beginners.wav"
- 
+   
+   
+   record()
+
+   
+   wavfile = "/home/shridhar/pocketsphinxtest/livewav.wav"
+
    speechRec = Decoder(hmm = hmdir, lm = lmdir, dict = dictd)
    wavFile = file(wavfile,'rb')
    speechRec.decode_raw(wavFile)
    ##print speechRec
    result = speechRec.get_hyp()
+   
    ##print "popat"
+   
+   
+   
    print "Recognised text from the converted video file"
    
    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
